daa013664dea12fec6f4ad3ff0c684803f1f5645
Dakshi Suri <dakshi884@gmail.com>
2018-02-19 17:38:48 +0530

    Extract text of cuurent document page

--- app/src/main/java/com/example/dakshi/busic/MainActivity.java
+++ app/src/main/java/com/example/dakshi/busic/MainActivity.java
@@ -17,8 +17,13 @@ import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
 import com.github.barteksc.pdfviewer.util.FitPolicy;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.PdfTextExtractor;
+import com.tom_roush.pdfbox.pdmodel.PDDocument;
+import com.tom_roush.pdfbox.pdmodel.PDPage;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
@@ -35,11 +40,14 @@ public class MainActivity extends AppCompatActivity {
     PDFView pdfView;
     Button select_file_button;
     String selected_file;
+    String path;
     SharedPreferences preferences;
     Gson gson;
     Type type;
     File file;
     int page_num;
+    PdfReader reader;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,12 +59,18 @@ public class MainActivity extends AppCompatActivity {
         gson=new Gson();
         type=new TypeToken<File>(){}.getType();
         // retrieving saved file
-        selected_file=preferences.getString("file_path",null);
+        selected_file=preferences.getString("file",null);
         page_num=preferences.getInt("number",1);
+        path=preferences.getString("path",null);
         // retrieve file object from file path
         if(selected_file!=null)
         {
             file=gson.fromJson(selected_file,type);
+            try {
+                reader = new PdfReader(path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             Toast.makeText(this, "not null", Toast.LENGTH_SHORT).show();
             load_pdf();
         }
@@ -75,21 +89,22 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, FilePickerActivity.class);
         intent.putExtra(FilePickerConstants.FILE, true);
         intent.putExtra(FilePickerConstants.TYPE, FilePickerConstants.MIME_PDF);
+        page_num=0;
         startActivityForResult(intent, MY_REQUEST_CODE);
     }
     public void load_pdf()
     {
         pdfView.fromFile(file)
                 .defaultPage(page_num)
-                .swipeHorizontal(true)
                 .enableDoubletap(true)
                 .pageFitPolicy(FitPolicy.BOTH)
                 .onPageChange(new OnPageChangeListener() {
                     @Override
-                    public void onPageChanged(int page, int pageCount) {
-
+                    public void onPageChanged(int page, int pageCount)
+                    {
                         page_num=page;
-                        Toast.makeText(MainActivity.this, ""+page+" "+pageCount, Toast.LENGTH_SHORT).show();
+                        pdfToString();
+                        //Toast.makeText(MainActivity.this, ""+page+" "+pageCount, Toast.LENGTH_SHORT).show();
                     }
                 })
                 .load();
@@ -104,6 +119,13 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK)
             {
                 Log.d("path", FilePickerUriHelper.getUriString(data));
+               // pdfToString(FilePickerUriHelper.getUriString(data));
+                path=FilePickerUriHelper.getUriString(data);
+                try {
+                    reader = new PdfReader(path);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 //selected_file=FilePickerUriHelper.getUriString(data);
                 file = FilePickerUriHelper.getFile(MainActivity.this, data);
                 selected_file=gson.toJson(file,type);
@@ -121,17 +143,38 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void pdfToString()
+    {
+        try {
+            String parsedText="";
+            for (int i = 0; i <1 ; i++) {
+                parsedText   = parsedText+ PdfTextExtractor.getTextFromPage(reader, page_num+1).trim()+"\n"; //Extracting the content from the different pages
+            }
+            Log.d("Text of document",parsedText);
+            //Toast.makeText(this, ""+parsedText, Toast.LENGTH_SHORT).show();
+            //System.out.println(parsedText);
+            //reader.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
     @Override
     protected void onSaveInstanceState(Bundle outState)
     {
         if(selected_file!=null)
         {
-            preferences.edit().putString("file_path",selected_file).apply();
+            preferences.edit().putString("path",path).apply();
+            preferences.edit().putString("file",selected_file).apply();
             preferences.edit().putInt("number",page_num).apply();
-            //preferences.edit().p("file_path",selected_file).apply();
-            Toast.makeText(this, ""+preferences.getString("file_path",null)+"  "+preferences.getInt("number",1), Toast.LENGTH_SHORT).show();
-            outState.putString("file_path",selected_file);
+            //Toast.makeText(this, ""+preferences.getString("file_path",null)+"  "+preferences.getInt("number",1), Toast.LENGTH_SHORT).show();
         }
         super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        reader.close();
     }
 }
